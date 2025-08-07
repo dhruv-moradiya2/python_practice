acd43905cf40bc522276a1721e378f25fb205f15
dhruv-moradiya2 <dhruv.unikworkdevops@gmail.com>
2025-08-07 18:06:10 +0530

    for loop as range work done

---
+++ Day_03/main.py
@@ -0,0 +1,42 @@
+# Print numbers from 1 to 100.
+# def check_num():
+#     for i in range(1,101):
+#         print(i)
+    
+# check_num()
+
+# Print even numbers between 1 to 50.
+# def check_even():
+#     for i in range(1, 51):
+#         if i % 2 == 0:
+#             print("even:",i)
+#         # else:
+#         #     print("not found")
+    
+# check_even()
+
+# Sum of first 50 natural numbers.
+# def check_sum():
+#     sum=0
+#     for i in range(51):
+#         sum +=i
+    
+#     print(sum)
+
+# check_sum()
+
+# Print multiplication table of any number.
+# def check_mul():
+#     val=5
+#     for i in range(1, 11):
+#         print(val * i)
+
+# check_mul()
+
+# Print all numbers divisible by 7 in a range.
+# def check_devi():
+#     for i in range(100):
+#         if i % 7 == 0:
+#             print("divisible:",i)
+
+# check_devi()
---
+++ Day_03/main.sh
@@ -0,0 +1,55 @@
+# Print numbers from 1 to 100.
+# function check_num() {
+#     for i in {1..100};
+#     do
+#         echo "number:${i}"
+#     done
+# }
+
+# check_num
+
+# Print even numbers between 1 to 50.
+# function check_even(){
+#     for val in {1..50};
+#     do
+#         if [[ $((val%2)) == 0 ]]; then
+#             echo "even: ${val}"
+#         fi
+#     done
+# }
+
+# check_even
+
+# Sum of first 50 natural numbers.
+# function check_num(){
+#     sum=0
+#     for i in {1..50};
+#     do
+#         sum=$((sum+i))
+#     done
+#     echo "${sum}"
+# }
+
+# check_num
+
+# Print multiplication table of any number.
+# function check_mul(){
+#     val=5
+#     for i in {1..10};
+#     do
+#         echo "$((val*i))"
+#     done
+# }
+
+# check_mul
+
+# Print all numbers divisible by 7 in a range.
+# function check_div(){
+#     for i in {1..100};do
+#         if [[ $((i%7)) == 0 ]]; then
+#             echo "divisible:${i}"
+#         fi
+#     done
+# }
+
+# check_div
