a8d8957e831d5ddc33f86ab44a6b847adee37290
dhruv-moradiya2 <dhruv.unikworkdevops@gmail.com>
2025-08-08 16:12:45 +0530

    just push value in github do in sunday

---
+++ Day_04/main.py
@@ -0,0 +1,13 @@
+# Print a square pattern using *.
+
+
+# Print a right-angled triangle using *.
+
+
+# Print multiplication tables from 1 to 10.
+
+
+# Create a basic pyramid pattern.
+
+
+# Print numbers in triangle shape.
---
+++ Day_04/main.sh
@@ -0,0 +1,15 @@
+# Print a square pattern using *.
+
+
+# Print a right-angled triangle using *.
+
+
+# Print multiplication tables from 1 to 10.
+
+
+# Create a basic pyramid pattern.
+
+
+# Print numbers in triangle shape.
+
+
