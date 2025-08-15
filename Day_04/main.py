76e3956b1ffc0924ca7dafe82e4d6918b9da9bbf
dhruv-moradiya2 <dhruv.unikworkdevops@gmail.com>
2025-08-15 17:01:57 +0530

    do less but good

--- Day_04/main.py
+++ Day_04/main.py
@@ -1,13 +1,57 @@
 # Print a square pattern using *.
+# def check_pattern():
+#     value=5
+    
+#     for row_i in range(value):
+#         for col_j in range(value):
+#             print("*", end=" ")
 
+#         print()
+
+# check_pattern()
 
 # Print a right-angled triangle using *.
+# def check_right():
+#     value=5
 
+#     for row_i in range(1, value+1):
+#         for col_j in range(row_i):
+#             print("*", end=" ")
+
+#         print()
+
+# check_right()
 
 # Print multiplication tables from 1 to 10.
+# def check_mul():
+    
+#     for i in range(1, 11):
+#         for j in range(1, 11):
+#             res= i * j 
+#             print(i,"*",j,"=",res)
+#         print()
 
+# check_mul()
 
 # Create a basic pyramid pattern.
+# def check_pyramid():
+#     value=5
 
+#     for i in range(1, value+1):
+#         print(" " * (value - i), end=" ")
+        
+#         print("*" * (2 * i - 1))
+
+# check_pyramid()
 
 # Print numbers in triangle shape.
+# def check_right():
+#     value=5
+
+#     for row_i in range(1, value+1):
+#         for col_j in range(1, row_i+1):
+#             print(col_j, end=" ")
+
+#         print()
+
+# check_right()
--- Day_04/main.sh
+++ Day_04/main.sh
@@ -1,13 +1,86 @@
 # Print a square pattern using *.
+# function check_num() {
+#     value=5
 
+#     for (( i=1; i<=value; i++));do
+#         for (( j=1; j <= value; j++ ));do
+#             echo -n "*"
+#         done
+#         echo 
+#     done
+
+#     # for row_i in {1..5}; do
+#     #     for col_j in {1..5}; do
+#     #         echo -n "*"
+#     #     done
+#     #     echo " " 
+#     # done
+
+# }
+
+# check_num
 
 # Print a right-angled triangle using *.
+# function check_right() {
+#     value=5
 
+#     for (( i=1; i<=value; i++)); do
+#         for (( j=0; j<i; j++)); do
+#             echo -n "*"
+#         done
+#     echo
+#     done
+# }
+
+# check_right
 
 # Print multiplication tables from 1 to 10.
+# function check_mul(){
 
+#     range=10
+
+#     for (( i=1; i<=range; i++ )); do
+#         for (( j=1; j<=range; j++ )); do
+#             mul=$(( i * j))
+#             echo "$i * $j = $mul"
+#         done
+#     echo 
+#     done
+
+#     # for i in {1..10}; do
+#     #     for j in {1..10}; do
+#     #         mul=$(( i * j))
+#     #         # print(i,"*",j,"=",res)
+#     #         echo "$i * $j = $mul"
+#     #     done
+#     # echo
+#     # done 
+# }
+
+# check_mul
 
 # Create a basic pyramid pattern.
+# function check_patt(){
+#     value=5
+
+#     for (( i=1; i<=value; i++ )); do
+#         echo -n $((" " * (${value} - i)))
+#         echo $(("*" * (2 * i -1)))
+#     done 
+# }
+
+# check_patt
+
+# def check_pyramid():
+#     value=5
+
+#     for i in range(1, value+1):
+#         print(" " * (value - i), end=" ")
+        
+#         print("*" * (2 * i - 1))
+
+# check_pyramid()
+
 
 
 # Print numbers in triangle shape.
